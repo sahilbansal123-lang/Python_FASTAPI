a1b2d30faddbceb7f62619480ee0bfe696948be4
sahilbansal123-lang <bansalsahil9053@gmail.com>
2024-12-14 12:14:27 +0530

    first commit

--- main.py
+++ main.py
@@ -4,8 +4,25 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data': {'name': 'sahil bansal'} }
+    return {'data': 'BLOG LIST'}
 
-@app.get('/about')
-def about():
-    return {'data': {'about page'} }
+## here it works fine
+@app.get('/blog/unpublished')
+def unpublished():  ## defining the type
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id:int):  ## defining the type
+    return {'data': id }
+
+
+## gives error because fast api read line by line
+# ## so it reads id first and gives error
+# @app.get('/blog/unpublished')
+# def unpublished():  ## defining the type
+#     return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}/comments')
+def commets(id):
+    return {'data': {'1', '2'}}  
