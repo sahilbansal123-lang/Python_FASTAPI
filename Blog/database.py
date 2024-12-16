e7ee9e8b63136f758aa7f885ca20a348475e57a9
sahilbansal123-lang <bansalsahil9053@gmail.com>
2024-12-16 12:21:21 +0530

    creating model and Table

--- Blog/database.py
+++ Blog/database.py
@@ -2,12 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:/// ./Blog.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./Blog.db'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
     connect_args={"check_same_thread":False}
     )
+
+
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autoCommit=False)
 
--- Blog/main.py
+++ Blog/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from . import schema
+from . import models
+from .database import engine
 
 
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
 
 @app.post('/blog')
 def create(request: schema.Blog):
