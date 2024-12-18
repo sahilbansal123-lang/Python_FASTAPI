cdbda03b664c249349b4471b7a024be39da595f7
sahilbansal123-lang <bansalsahil9053@gmail.com>
2024-12-18 12:16:07 +0530

    get blogs from database

--- Blog/main.py
+++ Blog/main.py
@@ -24,3 +24,12 @@ def create(request: schema.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}')
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
